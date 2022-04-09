7ee8a4ae5656521660882e0b85dec0c247619ee2
Keifer Blandon <keiferblandon48@gmail.com>
2022-04-09 16:26:41 -0700

    updated baseline network architecture

--- environment/instances.tf
+++ environment/instances.tf
@@ -14,7 +14,8 @@ resource "aws_instance" "bastion1" {
 
 #Redhat instance
 resource "aws_instance" "wpserver1" {
-  ami           = data.aws_ami.redhat.id
+  #ami           = data.aws_ami.redhat.id
+  ami = "ami-0b28dfc7adc325ef4"
   instance_type = "t3a.micro"
   subnet_id     = aws_subnet.webapp_1.id
   root_block_device {
@@ -28,7 +29,8 @@ resource "aws_instance" "wpserver1" {
 
 #Redhat instance
 resource "aws_instance" "wpserver2" {
-  ami           = data.aws_ami.redhat.id
+  #ami           = data.aws_ami.redhat.id
+  ami = "ami-0b28dfc7adc325ef4"
   instance_type = "t3a.micro"
   subnet_id     = aws_subnet.webapp_2.id
   root_block_device {
@@ -40,14 +42,15 @@ resource "aws_instance" "wpserver2" {
   }
 }
 
+# RDS Database instance
 resource "aws_db_instance" "default" {
   allocated_storage    = 10
   db_subnet_group_name = aws_db_subnet_group.rds_private.id
-  engine               = "PostgreSQL"
-  engine_version       = "11"
+  engine               = "postgres"
+  engine_version       = "11.15"
   instance_class       = "db.t3.micro"
-  db_name              = "RDS1"
+  name              = "RDS1"
   username             = var.db_username
   password             = var.db_password
-  skip_final_snapshot  = true
+  skip_final_snapshot  = true  #set to false for PROD
 }
--- environment/network.tf
+++ environment/network.tf
@@ -4,11 +4,6 @@
 
 resource "aws_vpc" "app_plane" {
   cidr_block = "10.1.0.0/16"
-
-  tags = {
-    Name    = "Application Plane"
-    Project = "Technical"
-  }
 }
 
 ###################################
@@ -19,7 +14,7 @@ resource "aws_lb" "application_lb" {
   name               = "Application-LB"
   internal           = false
   load_balancer_type = "application"
-  subnets            = local.public_subnets
+  subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
   #security_groups    = [aws_security_group.lb_sg.id] ## TODO: create custom ALB SG
 
   # Change to true once TF testing is complete
@@ -38,15 +33,15 @@ resource "aws_lb" "application_lb" {
 }
 
 resource "aws_lb_listener" "alb_listener_https" {
-  load_balancer_arn = aws_lb.front_end.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-FS-1-2-2019-08"
-  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+  load_balancer_arn = aws_lb.application_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  #ssl_policy        = "ELBSecurityPolicy-FS-1-2-2019-08"
+  #certificate_arn   = aws_acm_certificate.public_cert.arn
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.front_end.arn
+    target_group_arn = aws_lb_target_group.wp_server.arn
   }
 }
 
@@ -69,12 +64,34 @@ resource "aws_lb_target_group_attachment" "wpserver1" {
 ########################
 
 ## TODO: do data calls for AZ ids???
+## TODO: add IGW
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.app_plane.id
+}
+
+resource "aws_nat_gateway" "example" {
+  subnet_id     = aws_subnet.public_1.id
+  allocation_id = aws_eip.nat_eip.id
+
+  # Dependency added to rely on IGW
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
 
 # Public subnet
 resource "aws_subnet" "public_1" {
-  vpc_id     = aws_vpc.app_plane.id
-  cidr_block = "10.1.0.0/24"
+  vpc_id            = aws_vpc.app_plane.id
+  cidr_block        = "10.1.0.0/24"
   availability_zone = "us-west-2a"
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
 }
 
 # Public subnet
@@ -82,6 +99,11 @@ resource "aws_subnet" "public_2" {
   vpc_id            = aws_vpc.app_plane.id
   cidr_block        = "10.1.1.0/24"
   availability_zone = "us-west-2b"
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
 }
 
 #########################
@@ -119,8 +141,8 @@ resource "aws_subnet" "db_2" {
 #######################
 #### SUBNET GROUPS ####
 #######################
-
+## TODO: add secondary subnet in different AZ per requirments
 resource "aws_db_subnet_group" "rds_private" {
-  name       = "RDS Subnet Group"
-  subnet_ids = [aws_subnet.db_2.id]
+  name       = "rds-subnet-group"
+  subnet_ids = [aws_subnet.db_1.id, aws_subnet.db_2.id]
 }
