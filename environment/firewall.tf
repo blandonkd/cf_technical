9c4a21bfb0540a1678cc3d93c7e318408514a397
Keifer Blandon <keiferblandon48@gmail.com>
2022-04-09 16:26:01 -0700

    created alb sg

---
+++ environment/firewall.tf
@@ -0,0 +1,38 @@
+#########################
+#### SECURITY GROUPS ####
+#########################
+
+resource "aws_security_group" "alb_sg" {
+  name        = "alb_sg"
+  description = "Custome security group for ALB"
+  vpc_id      = aws_vpc.app_plane.id
+
+  ingress {
+    description      = "HTTPS from all"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+   ingress {
+    description      = "HTTP from all"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+
+
+##############
+#### NACL ####
+##############
