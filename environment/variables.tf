0e4957ca9b0d7f78002f584890e263f2fdbf4d0c
Keifer Blandon <keiferblandon48@gmail.com>
2022-04-09 12:44:22 -0700

    added variables for db for cred security

---
+++ environment/variables.tf
@@ -0,0 +1,13 @@
+# Set up variables
+variable db_username {
+  description = "Username for the database Master User"
+  type        = string
+  sensitive   = true
+}
+
+variable db_password {
+  description = "Password for the database Master User"
+  type        = string
+  sensitive   = true
+}
+
