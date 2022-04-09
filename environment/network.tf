d2094ca3aaa0e6f8ed07268721eb79c851ed4cff
Keifer Blandon <keiferblandon48@gmail.com>
2022-04-09 12:43:59 -0700

    added listener and tg to alb

--- environment/network.tf
+++ environment/network.tf
@@ -19,11 +19,13 @@ resource "aws_lb" "application_lb" {
   name               = "Application-LB"
   internal           = false
   load_balancer_type = "application"
-  #security_groups    = [aws_security_group.lb_sg.id] ## TODO: create ALB SG
   subnets            = local.public_subnets
+  #security_groups    = [aws_security_group.lb_sg.id] ## TODO: create custom ALB SG
 
-  enable_deletion_protection = true
+  # Change to true once TF testing is complete
+  enable_deletion_protection = false
 
+  # TODO (time permitting)
   # access_logs {
   #   bucket  = aws_s3_bucket.lb_logs.bucket
   #   prefix  = "test-lb"
@@ -34,6 +36,33 @@ resource "aws_lb" "application_lb" {
   #   Environment = "production"
   # }
 }
+
+resource "aws_lb_listener" "alb_listener_https" {
+  load_balancer_arn = aws_lb.front_end.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-FS-1-2-2019-08"
+  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.front_end.arn
+  }
+}
+
+resource "aws_lb_target_group" "wp_server" {
+  name     = "wpserver1-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.app_plane.id
+}
+
+resource "aws_lb_target_group_attachment" "wpserver1" {
+  target_group_arn = aws_lb_target_group.wp_server.arn
+  target_id        = aws_instance.wpserver1.id
+  port             = 80
+}
+
 
 ########################
 #### PUBLIC SUBNETS ####
@@ -85,4 +114,13 @@ resource "aws_subnet" "db_2" {
   vpc_id     = aws_vpc.app_plane.id
   cidr_block = "10.1.5.0/24"
   availability_zone = "us-west-2b"
+}
+
+#######################
+#### SUBNET GROUPS ####
+#######################
+
+resource "aws_db_subnet_group" "rds_private" {
+  name       = "RDS Subnet Group"
+  subnet_ids = [aws_subnet.db_2.id]
 }
