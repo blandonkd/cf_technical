3d71fa3caa341e62eca0c04cd45986d4b87036a8
Keifer Blandon <keiferblandon48@gmail.com>
2022-04-10 17:56:23 -0700

    formatting update

--- environment/variables.tf
+++ environment/variables.tf
@@ -1,11 +1,11 @@
 # Set up variables
-variable db_username {
+variable "db_username" {
   description = "Username for the database Master User"
   type        = string
   sensitive   = true
 }
 
-variable db_password {
+variable "db_password" {
   description = "Password for the database Master User"
   type        = string
   sensitive   = true
